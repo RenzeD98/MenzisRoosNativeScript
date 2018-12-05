5cbee8c13c2d3a0e2270e80dda370cae55edcdf7
RenzeD98 <renze.d98@gmail.com>
2018-12-05 09:41:51 +0100

    Initializing commit

---
+++ app/components/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!"></ActionBar>
+        <FlexboxLayout flexDirection="column" backgroundColor="#3c495e">
+            <Label class="message" :text="msg" flexGrow="1" backgroundColor="#43b883"/>
+            <Button text="Druk hier op"  @tap="buttonTap" />
+        </FlexboxLayout>
+    </Page>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'Check je geaardheid hier!'
+      }
+    },
+    methods: {
+        buttonTap() {
+            // this.msg = 'Ha je bent gay!';
+            this.$navigateTo('Home');
+        }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .grid-1 {
+        background-color: green;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+
+    .button {
+        height: 30;
+        width: 50;
+    }
+</style>
---
+++ app/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!"></ActionBar>
+            <Label class="message" text="Banaan" flexGrow="1" backgroundColor="#352343"/>
+    </Page>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
