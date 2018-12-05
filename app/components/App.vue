7cf3c4f806294e3269b671e357b33e84c10edb9f
RenzeD98 <renze.d98@gmail.com>
2018-12-05 14:28:07 +0100

    Added default routing example

--- app/components/App.vue
+++ app/components/App.vue
@@ -2,23 +2,41 @@
     <Page>
         <ActionBar title="Welcome to NativeScript-Vue!"></ActionBar>
         <FlexboxLayout flexDirection="column" backgroundColor="#3c495e">
-            <Label class="message" :text="msg" flexGrow="1" backgroundColor="#43b883"/>
-            <Button text="Druk hier op"  @tap="buttonTap" />
+            <Label v-for="message in messages" class="message" v-bind:text="msg" flexGrow="1" backgroundColor="#43b883"/>
+            <Button text="Druk hier op"  v-on:tap="$navigateTo(Home)" />
         </FlexboxLayout>
     </Page>
 </template>
 
 <script>
+  import Home from '../components/Home';
+  import axios from 'axios';
+
   export default {
+    mounted() {
+        this.sendRequest();
+    },
     data() {
       return {
-        msg: 'Check je geaardheid hier!'
+        Home: Home,
+        msg: 'Test',
+        messages: [],
       }
     },
     methods: {
-        buttonTap() {
-            // this.msg = 'Ha je bent gay!';
-            this.$navigateTo('Home');
+        sendRequest() {
+            var options = {
+                method: 'GET',
+                url: 'https://gateway-fra.watsonplatform.net/assistant/api/v1/workspaces/1cd29412-4a54-42d6-bdc8-c165cb69bb50/message?version=2018-09-20',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    "Authorization": "Basic YXBpa2V5OlkwdzNwcW5OamFCSDlzYmZOY1NPNFBtZkxhRC1NTEJHZ2ZsaG9HcTcxQlBG",
+                },
+            };
+            axios.get(options).then(response => {
+                this.messages = response.output.text
+            });
         }
     }
   }
