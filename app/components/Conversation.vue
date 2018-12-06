a951b5aa8baca515bb290d2ea3df560ea4e39734
rubendroogh <rubendroogh@hotmail.nl>
2018-12-06 14:56:46 +0100

    Created Conversation page.

---
+++ app/components/Conversation.vue
@@ -0,0 +1,65 @@
+<template>
+    <Page actionBarHidden="true" statusBarStyle="light">
+        
+    <GridLayout columns="*, 180, *" rows="*, 160, *, 40, 20">
+        <Label class="main-text" :text="msg" row="1" col="1" textWrap="true" />
+        <Button class="keyboard-button" @tap="keyboardTap" v-if="!inputToggle" row="3" col="1" />
+        <Label class="sent-text" :text="input" row="0" col="1" textWrap="true" v-if="inputSent" @tap="keyboardTap" />
+        <TextField class="input-field" v-model="input" hint="Enter text..." v-if="inputToggle" row="3" col="1" @returnPress="sendInput" returnKeyType="send" />
+    </GridLayout>
+    </Page>
+</template>
+
+<script>
+
+  export default {
+    mounted() {
+    },
+    data() {
+        return {
+            msg: 'Goedendag, waar kan ik u mee helpen?',
+            input: '',
+            inputToggle: false,
+            inputSent: false
+        }
+    },
+    methods: {
+        keyboardTap(){
+            this.inputToggle = true;
+        },
+        sendInput(){
+            this.inputSent = true;
+            this.inputToggle = false;
+        },
+        editInput(){
+            
+        }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+    @import url('https://fonts.googleapis.com/css?family=Roboto');
+    .main-text{
+        font-size: 18;
+        text-align: center;
+        font-family: 'Roboto', sans-serif;
+    }
+    .keyboard-button{
+        height: 40;
+        width: 40;
+        background-image: url('~/assets/images/keyboard.png');
+        background-repeat: no-repeat;
+    }
+    .sent-text{
+        font-family: 'Roboto', sans-serif;
+        font-style: italic;
+        text-align: center;
+        padding-top: 20;
+    }
+    .input-field{
+        font-family: 'Roboto', sans-serif;
+        font-style: italic;
+        border-bottom: 1px solid #ccc;
+    }
+</style>
