81b8acef5cd2a6cc3a97431530921a8c11e45ce4
rubendroogh <rubendroogh@hotmail.nl>
2018-12-08 15:50:36 +0100

    Added autofocus on input field

--- app/components/Conversation.vue
+++ app/components/Conversation.vue
@@ -5,7 +5,7 @@
         <Label class="main-text" :text="msg" row="1" col="1" textWrap="true" />
         <Button class="keyboard-button" @tap="keyboardTap" v-if="!inputToggle" row="3" col="1" />
         <Label class="sent-text" :text="input" row="0" col="1" textWrap="true" v-if="inputSent" @tap="keyboardTap" />
-        <TextField class="input-field" v-model="input" hint="Enter text..." v-if="inputToggle" row="3" col="1" @returnPress="sendInput" returnKeyType="send" />
+        <TextField ref="txtField" class="input-field" v-model="input" hint="Enter text..." v-if="inputToggle" row="3" col="1" @returnPress="sendInput" returnKeyType="send" />
     </GridLayout>
     </Page>
 </template>
@@ -26,6 +26,7 @@
     methods: {
         keyboardTap(){
             this.inputToggle = true;
+            this.$nextTick(() => this.$refs.txtField.nativeView.focus())
         },
         sendInput(){
             this.inputSent = true;
@@ -35,6 +36,14 @@
             
         }
     }
+    // Tests:
+    // 1: Juiste standaard watson bericht
+    // 2: Ophalen data uit local storage
+    // 3: Opslaan data in local storage
+    // 4: Goeie resultaat na een aantal vragen
+    // 5: Is op het juiste scherm
+    // 6: Geeft juiste formaat van vragen weer
+    // 7: Datum input
   }
 </script>
 
