a7b62379ab4d9666c6ce015511b258e62ef70503
rubendroogh <rubendroogh@hotmail.nl>
2018-12-10 14:44:37 +0100

    Removed unused App and Home components.

--- app/main.js
+++ app/main.js
@@ -1,7 +1,6 @@
 import Vue from 'nativescript-vue';
 import VueDevtools from 'nativescript-vue-devtools';
 
-import App from './components/App';
 import Conversation from './components/Conversation';
 import Introduction from './components/Introduction';
 
@@ -11,7 +10,6 @@ if(TNS_ENV !== 'production') {
 }
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production');
-
 
 new Vue({
   render: h => h('frame', [h(Introduction)])
