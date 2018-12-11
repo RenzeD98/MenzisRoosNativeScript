398098187fb78358af1dac101b32f97205cb7be6
rubendroogh <rubendroogh@hotmail.nl>
2018-12-11 13:15:26 +0100

    Added temporary markup

--- app/components/Introduction.vue
+++ app/components/Introduction.vue
@@ -1,11 +1,29 @@
 <template>
     <Page statusBarStyle="dark" androidStatusBarBackground="#105382" actionBarHidden="true" >
         <TabView :selectedIndex="selectedIndex" selectedTabTextColor="#105D94" tabBackgroundColor="#105D94" tabTextColor="#105D94" translateY="-45">
-            <TabViewItem title="Tab 1">
-                <Label text="Content for Tab 1" />
+            <TabViewItem>
+                <GridLayout columns="115, 115" rows="95, 115">
+                    <Label text="0,0" row="0" col="0" backgroundColor="#43b883"/>
+                    <Label text="0,1" row="0" col="1" backgroundColor="#1c6b48"/>
+                    <Label text="1,0" row="1" col="0" backgroundColor="#289062"/>
+                    <Label text="1,1" row="1" col="1" backgroundColor="#43b883"/>
+                </GridLayout>
             </TabViewItem>
-            <TabViewItem title="Tab 2">
-                <Label text="Content for Tab 2" />
+            <TabViewItem>
+                <GridLayout columns="43, 115" rows="115, 115">
+                    <Label text="0,0" row="0" col="0" backgroundColor="#43b883"/>
+                    <Label text="0,1" row="0" col="1" backgroundColor="#1c6b48"/>
+                    <Label text="1,0" row="1" col="0" backgroundColor="#289062"/>
+                    <Label text="1,1" row="1" col="1" backgroundColor="#43b883"/>
+                </GridLayout>
+            </TabViewItem>
+            <TabViewItem>
+                <GridLayout columns="115, 115" rows="115, 115">
+                    <Label text="0,0" row="0" col="0" backgroundColor="#43b883"/>
+                    <Label text="0,1" row="0" col="1" backgroundColor="#1c6b48"/>
+                    <Label text="1,0" row="1" col="0" backgroundColor="#289062"/>
+                    <Label text="1,1" row="1" col="1" backgroundColor="#43b883"/>
+                </GridLayout>
             </TabViewItem>
         </TabView>
     </Page>
