ae93b9043d90f0499b37984e3964a13d38d8a8d4
ruurdinho <ruurdsinnema@hotmail.com>
2018-12-12 11:42:38 +0100

    Deleted birthdate at profile creation

--- app/main.js
+++ app/main.js
@@ -7,7 +7,6 @@ import CreateProfile from './components/CreateProfile';
 
 var LS = require("nativescript-localstorage");
 var gender = LS.getItem('gender');
-var birthdate = LS.getItem('birthdate');
 
 if(TNS_ENV !== 'production') {
   Vue.use(VueDevtools);
@@ -15,7 +14,7 @@ if(TNS_ENV !== 'production') {
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production');
 
-if (gender && birthdate) {
+if (gender) {
   var StartComponent = Conversation;
 } else{
   var StartComponent = CreateProfile;
